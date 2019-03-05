8f5aac470ef8ac8c268ab013eaa96dd14ba5aa2a
liuguijie <qz18301089637@163.com>
2019-03-05 16:10:40 +0800

    人脸识别

--- app/src/main/java/com/example/facedetection/ShotActivity.java
+++ app/src/main/java/com/example/facedetection/ShotActivity.java
@@ -1,8 +1,11 @@
 package com.example.facedetection;
 
+import android.view.View;
+import android.widget.ImageView;
+
 import com.example.facedetection.base.BaseActivity;
 
-public class ShotActivity extends BaseActivity {
+public class ShotActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public int getLayoutId() {
         return R.layout.activity_shot;
@@ -10,6 +13,16 @@ public class ShotActivity extends BaseActivity {
 
     @Override
     public void onLoad() {
+        ImageView right_icon = findViewById(R.id.clude_icon);
+        right_icon.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.clude_icon:
+
+                break;
+        }
     }
 }
