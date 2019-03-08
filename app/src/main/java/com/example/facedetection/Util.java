136dca74479293a4410fc1c4c33ec5eabae48125
“yuejunfei” <451155047@qq.coom>
2019-03-08 13:45:00 +0800

    提交指定文件夹遍历图片工具类

--- app/src/main/java/com/example/facedetection/Util.java
+++ app/src/main/java/com/example/facedetection/Util.java
@@ -3,11 +3,56 @@ package com.example.facedetection;
 import android.app.Activity;
 import android.graphics.Point;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+
 public class Util {
     //获取屏幕的宽度
     public static int getScreenWidth(Activity activity) {
         Point point = new Point();
         activity.getWindowManager().getDefaultDisplay().getSize(point);
         return point.x;
+    }
+
+    /**
+     * 获取指定目录内所有文件路径
+     * @param dirPath 需要查询的文件目录
+     * @param _type 查询类型，比如mp3什么的
+     */
+    public static JSONArray getAllFiles(String dirPath, String _type) {
+        File f = new File(dirPath);
+        if (!f.exists()) {//判断路径是否存在
+            return null;
+        }
+
+        File[] files = f.listFiles();
+
+        if(files==null){//判断权限
+            return null;
+        }
+
+        JSONArray fileList = new JSONArray();
+        for (File _file : files) {//遍历目录
+            if(_file.isFile() && _file.getName().endsWith(_type)){
+                String _name=_file.getName();
+                String filePath = _file.getAbsolutePath();//获取文件路径
+                String fileName = _file.getName().substring(0,_name.length()-4);//获取文件名
+//                Log.d("LOGCAT","fileName:"+fileName);
+//                Log.d("LOGCAT","filePath:"+filePath);
+                try {
+                    JSONObject _fInfo = new JSONObject();
+                    _fInfo.put("name", fileName);
+                    _fInfo.put("path", filePath);
+                    fileList.put(_fInfo);
+                }catch (Exception e){
+                }
+            } else if(_file.isDirectory()){//查询子目录
+                getAllFiles(_file.getAbsolutePath(), _type);
+            } else{
+            }
+        }
+        return fileList;
     }
 }
